1b7b247de36ea4d001229cd3795d2b4645e24902
srinivaschamanthi <ch.srinivas1932@gmail.com>
2021-10-08 10:50:30 +0530

    ii

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./Header";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app-container">
         <Header/>
         <TriviaGame/>
       </div>
